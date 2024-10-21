ab4f9a59079ed917840a1be81caf63445d3a443c
Noam72T <9823@holbertonstudents.com>
2024-10-21 10:06:20 +0000

    Hello World - Exercice 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry Function
+*Print "with proper grammar, but the outcome is a piece of art,"
+*Return: 0.
+*/
+
+int main(void)
+{
+printf("Size of char : bytes\n", sizeof(char));
+printf("Size of an int: bytes\n", sizeof(int));
+printf("Size of a long int: bytes\n", sizeof(long int));
+printf("Size of a long long int: bytes\n", sizeof(long long int));
+printf("Size of a float: bytes\n", sizeof(float));
+return (0);
+}

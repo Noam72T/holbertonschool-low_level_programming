37886d811e50c4fad410da05cfba4e4b82eb01f1
Noam72T <9823@holbertonstudents.com>
2024-10-21 12:11:04 +0000

    Variable Exercice 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,28 +3,29 @@
 #include <stdlib.h>
 
 /**
-* main - Print if the number is negatif or positive or 0
-* Return: Always.                                                                                                                                                                                                                         
+* main - Print if the number is greater or less or 0.
+* Return: Always.
 */
 
 int main(void)
 {
-int n;
+int n, lastd;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+lastd = n % 10;
 
-if (n > 5)
+if (lastd > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 }
-else if (n == 0)
+else if (lastd == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
-else if (n < 6 )
+else if (lastd < 6 && lastd != 0)
 {
-printf("Last digit of %d is %d and less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastd);
 }
 return (0);
 }

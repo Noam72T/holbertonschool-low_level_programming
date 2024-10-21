c25ce301705f1679bdb9d7ff8f3b398b2b69053b
Noam72T <9823@holbertonstudents.com>
2024-10-21 10:10:19 +0000

    Hello World - Exercice 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of char : bytes\n", sizeof(char));
-printf("Size of an int: bytes\n", sizeof(int));
-printf("Size of a long int: bytes\n", sizeof(long int));
-printf("Size of a long long int: bytes\n", sizeof(long long int));
-printf("Size of a float: bytes\n", sizeof(float));
+printf("Size of char : %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }

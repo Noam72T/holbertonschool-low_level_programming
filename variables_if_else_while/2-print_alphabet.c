869077020a3603df00d9897864b1256f45636ba3
Noam72T <9823@holbertonstudents.com>
2024-10-21 12:34:40 +0000

    Variable Exercice 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Print if the Alphabet.
+* Return: Always.
+*/
+
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet ++)
+{
+putchar(alphabet);
+}
+return (0);
+}

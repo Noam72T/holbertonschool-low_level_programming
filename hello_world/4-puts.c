73a09f12232e46e90619fdca03b4c81421e269a1
Noam72T <9823@holbertonstudents.com>
2024-10-21 09:13:34 +0000

    Hello World - Exercice 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  *main - Print "Programming is like building a multilingual puzzle."
- *Return: 0.
+ *Return: Always 0.
  */
 
 
@@ -10,5 +10,5 @@ int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
   
-  return (0);
+  return 0;
 }

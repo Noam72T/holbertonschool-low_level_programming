ef450f885b724a0ea04c02974e4df9b35e700ad3
Noam72T <9823@holbertonstudents.com>
2024-10-22 08:09:27 +0000

    C - Functions, nested loops

---
+++ 0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Entry Function
+*Print "_putchar in newline"
+*Return: 0.
+*/
+
+int main()
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - Entry Function
+*Print "_putchar in newline"
+*Return: 0.
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}

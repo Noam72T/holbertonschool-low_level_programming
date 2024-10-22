d405ca9ce8648e3e2c8c7e185cb49ebe2cc8e211
Noam72T <9823@holbertonstudents.com>
2024-10-22 11:11:35 +0000

    C - Functions, nested loops Exercice 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+* _islower - Entry Function
+* @c: = The charcters
+* Print "_putchar in newline"
+* Return: 0.
+*/
+
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}

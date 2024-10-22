e6d0056e15b2f3a5ce6ea4222583b9fad2ab2aa2
Noam72T <9823@holbertonstudents.com>
2024-10-22 12:10:58 +0000

    C - Functions, nested loops Exercice 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* _abs - Entry Function
+* @n: random chiffre
+* Print "_putchar in newline"
+* Return: 0.
+*/
+
+int _abs(int n)
+{
+int y;
+y = abs(n);
+printf("%d", y);
+return (0);
+}

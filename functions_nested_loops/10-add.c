65c1c8a5ff450999efdbdb5f9b4b2788be02a5f5
Noam72T <9823@holbertonstudents.com>
2024-10-23 08:38:20 +0000

    C - Functions, nested loops Exercice 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* add - Entry Function
+* @a: int to be added to b
+* @b: int to be added to a
+* Return: c.
+*/
+
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}

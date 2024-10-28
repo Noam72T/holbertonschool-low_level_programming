a6395ff970e0e66aca1238949e5f5a24eb15aa0b
Noam72T <9823@holbertonstudents.com>
2024-10-28 10:20:02 +0000

    Exercice 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit - Entry Function
+* @c: Number checked
+* Return: 1 (Always)
+*/
+
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 10)
+{
+return (1);
+}
+return (0);
+}

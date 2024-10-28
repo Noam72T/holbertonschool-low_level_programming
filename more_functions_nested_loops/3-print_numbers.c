855feb5440e0bfe6de26c2a56b5f0e7c13758615
Noam72T <9823@holbertonstudents.com>
2024-10-28 10:50:08 +0000

    Exercice 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry Function
+* @num: Number checked
+* Return: 0 (Always)
+*/
+
+void print_numbers(void)
+{
+int num = 0;
+for (num = 48; num < 58; num++)
+{
+_putchar(num);
+}
+}

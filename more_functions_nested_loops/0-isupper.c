c3eb6c470b654e88dc4ca5850e44d8c9d37bb433
Noam72T <9823@holbertonstudents.com>
2024-10-28 10:00:21 +0000

    Exercice 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - Entry Function
+* @c: Number checked
+* Return: 0 (Always)
+*/
+
+int _isupper(int c);
+{
+if (c >= 'A' && c <= 'Z');
+return(1);
+}
+return(0);

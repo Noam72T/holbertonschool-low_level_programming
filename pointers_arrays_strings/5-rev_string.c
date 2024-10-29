07ee96003e0a802f09b228aaeabab20191c4650e
Noam72T <9823@holbertonstudents.com>
2024-10-29 14:23:23 +0000

    C - Pointers Exercice 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,19 +10,16 @@
 void rev_string(char *s)
 {
 char tmp;
-int i, len, len1;
+int i, len;
 len = 0;
 while (s[len] != '\0')
 {
 len++;
 }
-len1 = len - 1;
-printf("%s\n", s);
 for (i = 0; i < len / 2; i++)
 {
 tmp = s[i];
-s[i] = s[len1];
-s[len1--] = tmp;
+s[i] = s[len - 1 - i];
+s[len - 1 - i] = tmp;
 }
-printf("%s\n", s);
 }

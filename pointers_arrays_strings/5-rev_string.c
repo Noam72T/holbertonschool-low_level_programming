4d1107ea8f79aa9cd92e5e154de673d8cbd2011c
Noam72T <9823@holbertonstudents.com>
2024-10-29 13:38:26 +0000

    C - Pointers Exercice 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* rev_string - Entry Function
+* @s: reset The pointers
+* Return: returns nothing
+*/
+
+void rev_string(char *s)
+{
+int reverse = 0;
+int i;
+while (s[reverse] != '\0')
+{
+reverse++;
+}
+for (i = 0; s[i] != '\0'; i++)
+{
+putchar(s[i]);
+}
+putchar('\n');
+for (reverse -= 1; reverse >= 0; reverse--)
+{
+putchar(s[reverse]);
+}
+putchar('\n');
+}

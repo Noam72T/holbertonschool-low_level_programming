a01ec04ca0017884e608fe491c0b3bfa9188f30e
Noam72T <9823@holbertonstudents.com>
2024-10-30 09:28:55 +0000

    C - Pointers Exercice 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* puts_half - Entry Function
+* @str: reset The pointers
+* Return: returns nothing
+*/
+
+void puts_half(char *str)
+{
+int len, i;
+int startchar;
+while (str[len] != '\0')
+{
+len++;
+}
+startchar = len / 2;
+for (i = startchar; i < len; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}

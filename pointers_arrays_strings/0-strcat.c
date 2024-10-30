fcd960f36f69b7fa28b41746716234ccd7a21a48
Noam72T <9823@holbertonstudents.com>
2024-10-30 14:34:51 +0000

    C - More pointers Exercice 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* *_strcat - Entry Function
+* @src: reset The pointers
+* @dest: 2 pointers
+* Return: returns nothing
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[i] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}

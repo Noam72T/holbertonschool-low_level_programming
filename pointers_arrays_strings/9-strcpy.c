293ebe49e226c60bf5358e293b25f2849e6f118f
Noam72T <9823@holbertonstudents.com>
2024-10-30 13:03:00 +0000

    C - Pointers Exercice 10

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_strcpy - Entry Function
+* @dest: Pointers 1
+* @src: Pointers 2
+* Return: Value od the dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\n')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}

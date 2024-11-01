f8c969a9fcb7e0b3dc8ebeee31393aa213d1ddf3
Noam72T <9823@holbertonstudents.com>
2024-11-01 12:18:33 +0000

    C - More pointers Exercice 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* *leet - Entry Function
+* @str: reset The pointers
+* Return: returns nothing
+*/
+
+char *leet(char *str)
+{
+char lettre[] = "aAeEoOtTlL";
+char numb[] = "43071";
+int i;
+int j;
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; lettre[j] != '\0'; j++)
+{
+if (str[i] == lettre[j])
+{
+str[i] = numb[j];
+break;
+}
+}
+}
+return (str);
+}

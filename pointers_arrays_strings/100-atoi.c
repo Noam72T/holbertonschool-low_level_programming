55b662eb879dd5eb837f1236530bb6344995e6f4
Noam72T <9823@holbertonstudents.com>
2024-11-01 14:41:50 +0000

    C - Pointers Exercice 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,31 +5,38 @@
 
 /**
 * _atoi - Entry Function
-* @s reset The pointers
+* @s: reset The pointers
 * Return: returns nothing
 */
 
-
 int _atoi(char *s)
 {
-int sign = 1;
-int res = 0;
 int i = 0;
-while (s[i] == ' ') {
+int sign = 1;
+int result = 0;
+int digit_found = 0;
+while (s[i] == ' ')
+{
 i++;
 }
-while (s[i] == '-' || s[i] == '+') {
-if (s[i] == '-') {
+while (s[i] == '-' || s[i] == '+')
+{
+if (s[i] == '-')
+{
 sign *= -1;
 }
 i++;
 }
-while (s[i] >= '0' && s[i] <= '9') {
-if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7)) {
-return sign == 1 ? INT_MAX : INT_MIN;
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (result > (INT_MAX / 10) ||
+(result == (INT_MAX / 10) && (s[i] - '0') > (sign == 1 ? 7 : 8)))
+{
+return (sign == 1 ? INT_MAX : INT_MIN);
 }
-res = res * 10 + (s[i] - '0');
+result = result * 10 + (s[i] - '0');
+digit_found = 1;
 i++;
 }
-return (res * sign);
+return (digit_found ? result * sign : 0);
 }

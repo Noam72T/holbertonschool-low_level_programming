aed12c6a8573e9ee0a4032f441bf42f502544304
root <9823@holbertonstudents.com>
2024-11-02 11:11:20 +0000

    C - More pointers Exercice 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -15,6 +15,7 @@ int i = 0;
 int sign = 1;
 int result = 0;
 int digit_found = 0;
+int letter_count = 0;
 while (s[i] == ' ')
 {
 i++;
@@ -27,15 +28,26 @@ sign *= -1;
 }
 i++;
 }
-while (s[i] >= '0' && s[i] <= '9')
+while (s[i] != '\0')
 {
-if (result > (INT_MAX / 10) ||
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (result > (INT_MAX / 10) || 
 (result == (INT_MAX / 10) && (s[i] - '0') > (sign == 1 ? 7 : 8)))
 {
-return (sign == 1 ? INT_MAX : INT_MIN);
+return (sign == 1) ? INT_MAX : INT_MIN;
 }
 result = result * 10 + (s[i] - '0');
 digit_found = 1;
+}
+else if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+{
+letter_count++;
+}
+else if (digit_found)
+{
+break;
+}
 i++;
 }
 return (digit_found ? result * sign : 0);

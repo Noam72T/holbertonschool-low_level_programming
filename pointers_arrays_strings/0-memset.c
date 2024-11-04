a158b3a57d14dd9bd7f762467068dfbb344674c9
Elisea 76 <9823@holbertonstudents.com>
2024-11-04 10:34:08 +0100

    C - Even more pointers Exercice 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - Entry Function
+ * @s: Pointers
+ * @b: Octets
+ * @n: Not négatif number
+ * Return: returns nothing
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
+    return (s);
+}

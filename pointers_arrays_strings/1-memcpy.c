d9c222617d1b93c670087a81314700f6ad07c0cd
Elisea 76 <9823@holbertonstudents.com>
2024-11-04 10:52:18 +0100

    C - Even more pointers Exercice 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memcpy - Entry Function
+ * @dest: Pointers Dest
+ * @src: Pointers source
+ * @n: Not négatif number
+ * Return: returns nothing
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for(i = 0; i < n; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}

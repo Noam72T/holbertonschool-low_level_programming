c0d5424acf99cf07b2d967c8820f0a380dffdf7d
Noam72T <9823@holbertonstudents.com>
2024-11-06 09:42:14 +0000

    C - argc, argv Exercice 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - Function Main
+* @argc: - argc
+* @argv: - argv[]
+* Return: Always
+*/
+
+int main(int argc, char *argv[])
+{
+int sum = 0;
+int i, j;
+if (argc == 1)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Erreur\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}

dce4a6445b360bbe137d407ec42b5ff464868e2c
Noam72T <9823@holbertonstudents.com>
2024-11-06 08:54:56 +0000

    C - argc, argv Exercice 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Function Main
+* @argc: - argc
+* @argv: - argv[]
+* Return: Always
+*/
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}

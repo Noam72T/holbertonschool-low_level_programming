e96545b3f939826f29efea6ebf4b0a34d05a8992
Noam72T <9823@holbertonstudents.com>
2024-11-06 10:51:14 +0000

    C - argc, argv Exercice 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
 * main - Function Main
 * @argc: - argc
@@ -10,8 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-int num1, num2;
-int resultat;
+int num1;
+int num2;
 if (argc != 3)
 {
 printf("Erreur\n");
@@ -19,7 +18,6 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-resultat = num1 * num2;
-printf("%d\n", resultat);
+printf("%d\n", (num1 *num2));
 return (0);
 }

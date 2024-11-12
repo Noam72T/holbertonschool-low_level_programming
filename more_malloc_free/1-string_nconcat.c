fe9f491e4f555e0f979d6800f295b63d77ba624d
root <9823@holbertonstudents.com>
2024-11-12 13:52:54 +0000

    C - More malloc, free Exercice 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *string_nconcat - Function Main
+* @s1: - contains String
+* @s2: - String
+* @n: - int n
+* Return: Always
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *result;
+unsigned int len1, len2;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+result = malloc(len1 + n + 1);
+if (!result)
+return (NULL);
+memcpy(result, s1, len1);
+memcpy(result + len1, s2, n);
+result[len1 + n] = '\0';
+return (result);
+}

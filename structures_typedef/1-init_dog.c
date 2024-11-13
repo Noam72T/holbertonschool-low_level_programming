248ff206955543e7a61ade84d8c3d7636f6a67d6
root <9823@holbertonstudents.com>
2024-11-13 10:19:02 +0000

    C - Structures, typedef Exercice 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include "dog.h"
 
@@ -14,7 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
+{
 return;
+}
 d->name = name;
 d->age = age;
 d->owner = owner;

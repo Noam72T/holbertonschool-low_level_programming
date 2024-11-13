cd8491c629be70c2f55fe620c293b0356aff94f8
root <9823@holbertonstudents.com>
2024-11-13 08:56:59 +0000

    C - More malloc, free Exercice 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,21 +13,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *result;
-unsigned int len1, len2;
+int count, count1;
+int sign = n;
+char *ptr;
+int len1, len2;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-len1 = strlen(s1);
-len2 = strlen(s2);
-if (n >= len2)
-n = len2;
-result = malloc(len1 + n + 1);
-if (!result)
+for (len1 = 0; s1[len1] != '\0'; len1++)
+;
+for (len2 = 0; s2[len2] != '\0'; len2++)
+;
+if (sign >= len2)
+{
+sign = len2;
+ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+}
+else
+ptr = malloc(sizeof(char) * (len1 + n + 1));
+if (ptr == NULL)
+{
 return (NULL);
-memcpy(result, s1, len1);
-memcpy(result + len1, s2, n);
-result[len1 + n] = '\0';
-return (result);
+}
+for (count = 0; count < len1; count++)
+{
+ptr[count] = s1[count];
+}
+for (count1 = 0; count1 < sign; count1++)
+{
+ptr[count++] = s2[count1];
+}
+ptr[count++] = '\0';
+return (ptr);
 }

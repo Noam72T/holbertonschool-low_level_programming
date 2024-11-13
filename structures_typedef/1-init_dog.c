81a7405964af338b57a037a2df731742c56f17a1
root <9823@holbertonstudents.com>
2024-11-13 10:07:18 +0000

    C - Structures, typedef Exercice 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* init_dog - Structure de la fonction
+* @d: - declaration
+* @name: - Name
+* @age: - Age du chien
+* @owner: - Maitre du chien
+* Return: return pointer to struct
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}

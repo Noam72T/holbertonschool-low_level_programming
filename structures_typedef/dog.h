94ed61ae5a29cb26ae6dac44c4d78f4fa8c080b0
root <9823@holbertonstudents.com>
2024-11-13 09:52:36 +0000

    C - Structures, typedef Exercice 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure de la fonction
+* @name: - Name
+* @age: - Age du chien
+* @owner: - Maitre du chien
+*/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} Dog;
+
+#endif

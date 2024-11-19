716d454b93940fc75c5e945928ca31c5b27e8f5c
Noam72T <9823@holbertonstudents.com>
2024-11-19 12:54:54 +0000

    C - Functions Pointers Exercice 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
-* print_name - Function Main
-* @name: - contains Char
-* @f: - void
-* Return: Always
+* print_name - Prints a name
+* @name: The name to prints
+* @f: Pointer to function
+* Return: Nothing
 */
 
 void print_name(char *name, void (*f)(char *))

2b02d10c455deee967ba35c7e3e14a83738d70ba
Noam72T <9823@holbertonstudents.com>
2024-11-20 08:34:51 +0000

    C - Variadic functions Exercice 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,10 +12,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
-int sum;
+int sum = 0;
 va_list list;
+if (n == 0)
+return (0);
 va_start(list, n);
-for (i = 0, sum = 0; i < n; i++)
+for (i = 0; i < n; i++)
 sum += va_arg(list, int);
 va_end(list);
 return (sum);

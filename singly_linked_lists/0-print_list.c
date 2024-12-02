8b935c95d15e6964622585f529d73ce5af0984ac
Elisea 76 <9823@holbertonstudents.com>
2024-12-02 12:00:38 +0100

    C - Singly linked lists Exercice 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_list - Function for prints All Elements
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t i = 0;
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[%d] %s\n", 0, "(nil)");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}

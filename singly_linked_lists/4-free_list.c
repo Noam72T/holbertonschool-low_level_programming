370153654e95e8fd882626749c05d7767841502b
Noam72T <9823@holbertonstudents.com>
2024-12-03 14:26:37 +0100

    C - Singly linked lists Exercice 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free list
+ * @head: pointers of the link
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}

d99169a7efc655197481683aa69e0aff79bf45d4
Noam72T <9823@holbertonstudents.com>
2024-12-03 10:57:22 +0100

    C - Singly linked lists Exercice 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_node - Add New node 
+* @head: Header to the pointer
+* @str: String to duplicate .
+* Return: Addr new node .
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    int count;
+
+   
+    for (count = 0; str[count] != '\0'; count++)
+        ;
+
+    
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return (NULL); 
+
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node); 
+        return (NULL);
+    }
+
+  
+    new_node->len = count;        
+    new_node->next = *head;    
+
+    
+    *head = new_node;
+
+    return (new_node); 
+}
+

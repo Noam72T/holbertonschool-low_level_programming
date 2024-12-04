5dc4151fdff6db20dbf8dfc39445e25da0be6a33
Noam72T <9823@holbertonstudents.com>
2024-12-04 09:30:49 +0100

    C - Singly linked lists Exercice 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,21 +6,18 @@
 /**
 * add_node_end - Add New node end
 * @head: Header to the pointer
-* @str: String to duplicate .
-* Return: Addr new node .
+* @str: String to duplicate
+* Return: Addr new node
 */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
+	list_t *new_node, *temp;
 	unsigned int len = 0;
 
-	if (str == NULL)
-		return (NULL);
 
 	while (str[len])
 		len++;
-
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -36,18 +33,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = len;
 	new_node->next = NULL;
 
-
+	
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	else
-	{
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new_node;
-	}
+
+	
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+
+	temp->next = new_node;
 
 	return (new_node);
 }

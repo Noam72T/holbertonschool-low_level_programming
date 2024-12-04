b76ec8e52858edd32b1446ae72cbcaa25a0ea82b
Noam72T <9823@holbertonstudents.com>
2024-12-04 15:50:49 +0100

    C - Doubly linked lists Exercice 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+* dlistint_len - Function for prints All Elements
+* @h: pointers contains of the header
+* Return: Number Of elements
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+
+        h = h->next;
+	}
+
+	return (count);
+}

c8cb623215c3f51c0460d453e498003ee5912dab
Noam72T <9823@holbertonstudents.com>
2024-12-04 09:03:17 +0100

    C - Singly linked lists Exercice 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,45 +10,55 @@
 * Return: Addr new node .
 */
 
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Ajoute un nouveau nœud à la fin d'une liste chaînée list_t.
+ * @head: Double pointeur vers la tête de la liste.
+ * @str: Chaîne à dupliquer et à ajouter dans le nouveau nœud.
+ *
+ * Return: Adresse du nouveau nœud, ou NULL en cas d'échec.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *temp;
-    int count;
+	list_t *new_node, *temp;
+	unsigned int len = 0;
 
-   
-    for (count = 0; str[count] != '\0'; count++)
-        ;
+	if (str == NULL)
+		return (NULL);
 
-    
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL); 
+	
+	while (str[len])
+		len++;
 
+	
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node); 
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = len;
+	new_node->next = NULL;
 
-  
-    new_node->len = count;        
-    new_node->next = NULL;    
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new_node;
+	}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
-
-
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
-
-    
-    temp->next = new_node;
-
-    return (new_node);
+	return (new_node);
 }
+

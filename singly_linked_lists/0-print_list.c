e493c4312be39b5255d6e38c35ddf11621eb9399
Noam72T <9823@holbertonstudents.com>
2024-12-04 15:13:45 +0100

    C - Singly linked lists Exercice 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,24 +1,31 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a list
- * @h: pointer to the head list
- * Return: the number of nodes
- */
+* print_list - Function for prints All Elements
+* @h: pointers contains string
+*
+* Return: Number Of elements
+*/
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
+		if (h->str != NULL)
+		{
 			printf("[%u] %s\n", h->len, h->str);
-
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		count++;
 		h = h->next;
-		node_count++;
 	}
-	return (node_count);
+	return (count);
 }

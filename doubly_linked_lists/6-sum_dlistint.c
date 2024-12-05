3f9cf70a369695de13d4bf9fc80bc6c44fc7d28b
root <root@Pc-Dev>
2024-12-05 11:49:42 +0100

    C - Doubly linked lists Exercice 6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -13,8 +13,8 @@ int sum_dlistint(dlistint_t *head)
 
     while (head != NULL)
     {
-        sum += head->n;
-        head = head->next;
+    sum += head->n;
+    head = head->next;
     }
     return (sum);
 }

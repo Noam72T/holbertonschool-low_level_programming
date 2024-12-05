4e6d9eabbc3af14fed11a8df970b1dc355c32131
Noam72T <9823@holbertonstudents.com>
2024-12-05 09:39:20 +0000

    C - Doubly linked lists Exercice 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,39 +2,40 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - Function of the node
- * @head: Double pointer head.
- * @n: new value of the interger.
- * Return: Addresse of the element
- */
+* add_dnodeint_end - Function of the node
+* @head: Double pointer head.
+* @n: new value of the interger.
+* Return: Addresse of the element
+*/
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *temp;
+{
+	dlistint_t *new_node, *temp;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    temp = *head;
-    while (temp->next != NULL)
-        temp = temp->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    temp->next = new_node;
-    new_node->prev = temp;
+	temp->next = new_node;
+	new_node->prev = temp;
 
-    return (new_node);
+	return (new_node);
 }

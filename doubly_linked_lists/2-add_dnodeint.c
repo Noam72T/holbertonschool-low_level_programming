ed6eb8be5c171a36a7f15deea17987cc71db8bf8
Noam72T <9823@holbertonstudents.com>
2024-12-05 09:31:15 +0000

    C - Doubly linked lists Exercice 2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
-* add_dnodeint - Function of the node 
+* add_dnodeint - Function of the node
 * @head: Double pointer head.
 * @n: new value of the interger.
 * Return: Addresse of the element
@@ -14,22 +14,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
-
 	if (*head != NULL)
 		(*head)->prev = new_node;
 
-
 	*head = new_node;
-
 	return (new_node);
 }

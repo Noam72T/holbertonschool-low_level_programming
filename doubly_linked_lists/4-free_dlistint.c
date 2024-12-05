1fbd0ca70aa3ab67679563a73cfcf6f4767c4cef
Elisea 76 <9823@holbertonstudents.com>
2024-12-05 11:03:33 +0100

    C - Doubly linked lists Exercice 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* add_dnodeint_end - Function of the node
+* @head: Double pointer head.
+* @n: new value of the interger.
+* Return: Addresse of the element
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+
+    dlistint_t *temp;
+
+while (head != NULL)
+{
+    temp = head;
+    head = head->next;
+    free(temp);
+}
+}

6beade94353c19e70277b6b789f02dbbc644878b
Elisea 76 <9823@holbertonstudents.com>
2024-12-05 11:07:19 +0100

    C - Doubly linked lists Exercice 4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -10,13 +10,12 @@
 
 void free_dlistint(dlistint_t *head)
 {
-
     dlistint_t *temp;
 
-while (head != NULL)
-{
-    temp = head;
-    head = head->next;
-    free(temp);
-}
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
 }

948526abb9d3004100c7f701b1e93ba6e2e24ff9
Noam72T <9823@holbertonstudents.com>
2024-12-05 13:17:59 +0100

    C - Bit manipulation Exercice 0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint(const char *b)
+ * @b: pointers
+ * Return: Resultat
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int result = 0;
+
+    if (b == NULL)
+    {
+        return 0;
+    }
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        if (b[i] != '0' && b[i] != '1')
+        {
+            return 0;
+        }
+        result = result * 2 + (b[i] - '0');
+    }
+    return result;
+}

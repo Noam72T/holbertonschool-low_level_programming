935d81098616000c9fe7c3ee3f04352f22ae30ab
Elisea 76 <9823@holbertonstudents.com>
2024-12-05 11:19:00 +0100

    C - Doubly linked lists Exercice 5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - Function of the node
+* @head: Double pointer head.
+* @index: index of the node
+* Return: Addresse of the element
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i = 0;
+
+    while (head != NULL)
+    {
+        if (i == index)
+            return (head);
+        head = head->next;
+        i++;
+    }
+
+    return (NULL);
+}

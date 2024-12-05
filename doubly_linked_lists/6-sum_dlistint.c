9974a4ed0fdbd8b624db9f38788b3cb43a7ce538
root <root@Pc-Dev>
2024-12-05 11:48:48 +0100

    C - Doubly linked lists Exercice 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Function of the node
+ * @head: Double pointer head.
+ * Return: Addresse of the element
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+    return (sum);
+}

c51bd3f807b22e7913bca921cd19b59dfb6df2f9
Noam72T <9823@holbertonstudents.com>
2024-12-06 09:35:54 +0100

    C - Doubly linked lists Exercice 6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,19 +2,17 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - Function of the node
- * @head: Double pointer head.
- * Return: Addresse of the element
- */
+* sum_dlistint - Function of the node
+* @head: Double pointer head.
+* Return: Addresse of the element
+*/
 
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
-
-    while (head != NULL)
-    {
-    sum += head->n;
-    head = head->next;
-    }
-    return (sum);
+int sum;
+dlistint_t *tmp;
+tmp = head;
+for (sum = 0; tmp; tmp = tmp->next)
+sum += tmp->n;
+return (sum);
 }

8c45f11993d6bd82a473ab879aeb38a2d7f08152
Noam72T <9823@holbertonstudents.com>
2024-12-06 09:00:06 +0000

    C - Bit manipulation Exercice 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,15 +7,16 @@
 * @m: Pointer 2 number.
 * Return: Count.
 */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int result;
 unsigned int count = 0;
-result = n ^ m; 
+result = n ^ m;
 while (result)
 {
-count += result & 1; 
-result >>= 1;        
+count += result & 1;
+result >>= 1;
 }
 return (count);
 }

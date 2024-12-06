8f076e7295f5d53de0170debe13769c77fe5d72e
Noam72T <9823@holbertonstudents.com>
2024-12-06 09:38:17 +0100

    C - Bit manipulation Exercice 3

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -5,17 +5,17 @@
 #include "main.h"
 
 /**
- * get_bit -Entry Function
- * @n: pointers
- * @index: index bit
- * Return: Print the result
- */
+* get_bit -Entry Function
+* @n: pointers
+* @index: index bit
+* Return: Print the result
+*/
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= 64)
-    {
-        return (-1);
-    }
-    return ((n >> index) & 1);
+if (index >= 64)
+{
+return (-1);
+}
+return ((n >> index) & 1);
 }

3be392d831d45ae0adfffe67df6e367f4c71d655
Noam72T <9823@holbertonstudents.com>
2024-12-08 13:35:58 +0100

    C - File I/O Exercice 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,22 +13,41 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buf;
-int lenRead, lenWrite;
-if (filename == NULL)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buf = malloc(letters * sizeof(char));
-if (buf == NULL)
-return (0);
-lenRead = read(fd, buf, letters);
-lenWrite = write(STDOUT_FILENO, buf, lenRead);
-if (lenWrite != lenRead && lenWrite == -1)
-return (0);
-free(buf);
-close(fd);
-return (lenRead);
+	int fd;
+	ssize_t bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
 }

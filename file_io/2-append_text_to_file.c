dd11de4fcd832674fce2517a14c338b9ec59687e
Noam72T <9823@holbertonstudents.com>
2024-12-08 13:16:44 +0100

    C - File I/O Exercice 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,35 +10,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, i = 0;
-    ssize_t bytes_written;
-
-    
-    if (filename == NULL)
-        return (-1);
-
- 
-    if (text_content == NULL)
-        text_content = "";
-
-   
-    while (text_content[i] != '\0')
-        i++;
-
-    
-    file = open(filename, O_WRONLY | O_APPEND);
-    if (file == -1)
-        return (-1);
-
-   
-    bytes_written = write(file, text_content, i);
-    if (bytes_written == -1)
-    {
-        close(file);
-        return (-1);
-    }
-
-    close(file);
-
-    return (1);
+int file, i = 0;
+ssize_t bytes_written;
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+text_content = "";  
+while (text_content[i] != '\0')
+i++; 
+file = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
+return (-1);
+bytes_written = write(file, text_content, i);
+if (bytes_written == -1)
+{
+close(file);
+return (-1);
+}
+close(file);
+return (1);
 }

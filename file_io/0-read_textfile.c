b8cf53ac6fb395e68f5c2a619ab53e4e0ae5632f
Noam72T <9823@holbertonstudents.com>
2024-12-08 13:32:05 +0100

    C - File I/O Exercice 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * read_textfile - Function Read
  * @filename: pointers
@@ -11,42 +12,35 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int files;
-    ssize_t let, read_bytes;
-    char *text;
-
-    if (filename == NULL)
-        return (0);
-
-    text = malloc(letters);
-    if (text == NULL)
-        return (0);
-
-    files = open(filename, O_RDONLY);
-    if (files == -1)
-    {
-        free(text);
-        return (0);
-    }
-
-    let = read(files, text, letters);
-    if (let == -1)
-    {
-        free(text);
-        close(files);
-        return (0);
-    }
-
-    read_bytes = write(STDOUT_FILENO, text, let);
-    if (read_bytes == -1 || read_bytes != let)
-    {
-        free(text);
-        close(files);
-        return (0);
-    }
-
+int files;
+ssize_t let, read_bytes;
+char *text;
+if (filename == NULL)
+    return (0);
+text = malloc(letters);
+if (text == NULL)
+    return (0);
+files = open(filename, O_RDONLY);
+if (files == -1)
+{
+    free(text);
+    return (0);
+}
+let = read(files, text, letters);
+if (let == -1)
+{
     free(text);
     close(files);
-
-    return (read_bytes);
+    return (0);
+}
+read_bytes = write(STDOUT_FILENO, text, let);
+if (read_bytes == -1 || read_bytes != let)
+{
+    free(text);
+    close(files);
+    return (0);
+}
+free(text);
+close(files);
+return (read_bytes);
 }

01ccfc550eeec25b8d366aa62cfc04462c391f36
Noam72T <9823@holbertonstudents.com>
2024-12-08 14:00:37 +0100

    C - File I/O Exercice 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,45 +6,72 @@
 
 #define BUFFER_SIZE 1024
 
+void error_exit(const char *msg, int code, const char *file);
+void close_file(int fd);
+
 /**
- * main - Function main
- * @argc: arg counter
- * @argv: arg value
- * Return: 0 success
+ * main - Function main.
+ * @argc: Args Counter.
+ * @argv: Args Value.
+ * Return: 0 on success
  */
+
 int main(int argc, char *argv[])
 {
-int file_from, file_to, bytes = 1024, num;
-char buf[1024];
-if (argc != 3)
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+	int file_from, file_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit("Usage: cp file_from file_to", 97, NULL);
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		error_exit("Error: Can't read from file", 98, argv[1]);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		error_exit("Error: Can't write to", 99, argv[2]);
+
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+			error_exit("Error: Can't write to", 99, argv[2]);
+	}
+	if (bytes_read == -1)
+		error_exit("Error: Can't read from file", 98, argv[1]);
+
+	close_file(file_from);
+	close_file(file_to);
+
+	return (0);
 }
-file_to = creat(argv[2], 0664);
-if (file_to == -1)
+/**
+ * error_exit - Function Error and exit.
+ * @msg: Error mess.
+ * @code: Code Error.
+ * @file: Name of the file.
+ */
+
+void error_exit(const char *msg, int code, const char *file)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from), exit(99);
+	if (file)
+		dprintf(STDERR_FILENO, "%s %s\n", msg, file);
+	else
+		dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(code);
 }
-while (bytes == 1024)
+
+/**
+ * close_file - Function close file.
+ * @fd: file to close.
+ */
+void close_file(int fd)
 {
-bytes = read(file_from, buf, 1024);
-if (bytes == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-num = write(file_to, buf, bytes);
-if (num < bytes)
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-}
-if (close(file_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
-if (close(file_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-return (0);
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
 }

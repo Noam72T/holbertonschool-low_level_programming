c0f2e4a011172a4b7987a4fe3895139eecaaf33b
Noam72T <9823@holbertonstudents.com>
2024-12-08 13:28:00 +0100

    C - File I/O Exercice 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,37 +13,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int i = 0, files;
-    ssize_t bytes_written;
-
-
-    if (filename == NULL)
-        return (-1);
-
-
-    if (text_content == NULL)
-        text_content = "";
-
-   
-    while (text_content[i] != '\0')
-    {
-        i++;
-    }
-
- 
-    files = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (files == -1)
-        return (-1);
-
-
-    bytes_written = write(files, text_content, i);
-    if (bytes_written == -1)
-    {
-        close(files);
-        return (-1);
-    }
-
-    close(files);
-
-    return (1);
+int i = 0, files;
+ssize_t bytes_written;
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+text_content = "";
+while (text_content[i] != '\0')
+{
+i++;
+}
+files = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (files == -1)
+return (-1);
+bytes_written = write(files, text_content, i);
+if (bytes_written == -1)
+{
+close(files);
+return (-1);
+}
+close(files);
+return (1);
 }
